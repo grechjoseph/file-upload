6e7a7c2b02ca30b675a19c42e1ac29c21ea4899c
jgrech <joseph.grech@melitaltd.com>
2022-03-01 13:39:14 +0100

    Streamed upload using RestTemplate.

--- src/main/java/com/jg/largefileupload/FileUploadController.java
+++ src/main/java/com/jg/largefileupload/FileUploadController.java
@@ -4,58 +4,33 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.DecimalFormat;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class FileUploadController {
 
-    private final SelfApiClient selfApiClient;
-
     @Value("${upload.directory:files}")
     private String uploadDirectory;
 
-    @PostMapping("/bridge-upload")
-    public String handleBridgeUpload(@RequestParam("file") final MultipartFile file) {
-        final RestTemplate restTemplate = new RestTemplate();
-        final SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setBufferRequestBody(false);
-        restTemplate.setRequestFactory(requestFactory);
+    @Value("${feign.self-client.url:http://file-api:8080}")
+    private String selfClientUrl;
 
-        log.info("Bridge upload started.");
-        final String uploadUrl = "http://localhost:8080/upload";
-        final HttpMethod httpMethod = HttpMethod.POST;
-        final MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
-        bodyMap.add("file", file);
-        final HttpHeaders header = new HttpHeaders();
-        final HttpEntity<MultiValueMap> request = new HttpEntity<>(bodyMap, header);
-
-        final String response = restTemplate.exchange(uploadUrl, httpMethod, request, String.class).getBody();
-        log.info("Bridge upload ended.");
-
-        return response;
-
-
-//        log.info("Bridge upload started.");
-//        final String result = selfApiClient.fileUpload(file);
-//        log.info("Bridge upload ended.");
-//        return result;
-    }
+    // Single Microservice
 
     @PostMapping("/upload")
     public String handleUpload(@RequestParam("file") final MultipartFile file) throws Exception {
@@ -67,6 +42,57 @@ public class FileUploadController {
         log.info("Writing InputStream to OutputStream.");
         while ((bytesRead = inputStream.read(buffer)) != -1) {
             outputStream.write(buffer, 0, bytesRead);
+        }
+        log.info("Finished writing InputStream to OutputStream.");
+        IOUtils.closeQuietly(inputStream);
+        IOUtils.closeQuietly(outputStream);
+
+        return "success!";
+    }
+
+    // Bridged (two microservices)
+
+    // Microservice 1 Endpoint
+    @PostMapping("/bridge-upload-from")
+    public String bridgeUploadFrom(@RequestParam("file") final MultipartFile file) throws IOException {
+        final RestTemplate restTemplate = new RestTemplate();
+
+        final SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setBufferRequestBody(false);
+        requestFactory.setConnectTimeout(1000 * 120);
+        requestFactory.setReadTimeout(1000 * 120);
+        // upload 16KB at a time.
+        requestFactory.setChunkSize(16 * 1024);
+        restTemplate.setRequestFactory(requestFactory);
+
+        return restTemplate.postForEntity(selfClientUrl + "/bridge-upload-to" +
+                        "?file-name=" + file.getOriginalFilename() +
+                        "&file-size=" + file.getSize() +
+                        "&content-type=" + file.getContentType(),
+                new InputStreamResource(file.getInputStream()),
+                String.class)
+                .getBody();
+    }
+
+    // Microservice 2 Endpoint
+    @PostMapping("/bridge-upload-to")
+    public String bridgeUploadTo(@RequestParam("file-name") final String fileName,
+                                 @RequestParam("file-size") final long fileSize,
+                                 @RequestParam("content-type") final String contentType,
+                                 @RequestBody final InputStreamResource file) throws Exception {
+        final InputStream inputStream = file.getInputStream();
+        final OutputStream outputStream = new FileOutputStream(uploadDirectory + "/" + fileName);
+
+        // Read 16Kb at a time.
+        byte[] buffer = new byte[16 * 1024];
+        int totalBytesRead = 0;
+        int bytesRead;
+        log.info("Writing InputStream to OutputStream.");
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+            totalBytesRead += bytesRead;
+            final Double percentage = (100d * (double) totalBytesRead) / (double) fileSize;
+            System.out.println(new DecimalFormat("0.00").format(percentage) + "%");
         }
         log.info("Finished writing InputStream to OutputStream.");
         IOUtils.closeQuietly(inputStream);
